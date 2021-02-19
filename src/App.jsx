90afe14ddfe1752621c7ef635ee29dd86b1b8bf5
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-02-19 15:26:43 +0530

    add convertToGif function

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
 
   const [ready, setReady] = useState(false);
   const [video, setVideo] = useState();
+  const [gif, setGif] = useState();
 
   const load = async () => {
     await ffmpeg.load();
@@ -18,6 +19,21 @@ function App() {
 
   }, []);
 
+  const convertToGif = async () => {
+    // Write the file to memory
+    ffmpeg.FS('writeFile', 'test.mp4', await fetchFile(video));
+
+    // Run the FFMpeg command
+    await ffmpeg.run('-i', 'test.mp4', '-t', '2.5', '-ss', '2.0', '-f', 'gif', 'out.gif');
+
+    // Read the result
+    const data = ffmpeg.FS('readFile', 'out.gif');
+
+    //Create a URL
+    const url = URL.createObjectURL(new Blob([data.buffer], { type: 'image/gif' }));
+    setGif(url);
+  };
+
   return ready ? (
     <div className="App">
       {video && <video
@@ -26,6 +42,13 @@ function App() {
                   src={URL.createObjectURL(video)}>
                 </video>}
       <input type="file" onChange={(e) => setVideo(e.target.files?.item(0))} />
+
+      <h3>Result</h3>
+
+      <button onClick={convertToGif}>Convert</button>
+
+      { gif && <img src={gif} width='250' /> }
+
     </div>
   ) : (
     <p>Loading...</p>
