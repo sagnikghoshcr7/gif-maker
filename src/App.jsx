07cc91d0134ca1eef75a97093b87ed79b1466996
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-02-19 15:39:51 +0530

    update snowpack & code format

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
 const ffmpeg = createFFmpeg({ log: true });
 
 function App() {
-
   const [ready, setReady] = useState(false);
   const [video, setVideo] = useState();
   const [gif, setGif] = useState();
@@ -13,14 +12,14 @@ function App() {
   const load = async () => {
     await ffmpeg.load();
     setReady(true);
-  };
+  }
 
   useEffect(() => {
-
-  }, []);
+    load();
+  }, [])
 
   const convertToGif = async () => {
-    // Write the file to memory
+    // Write the file to memory 
     ffmpeg.FS('writeFile', 'test.mp4', await fetchFile(video));
 
     // Run the FFMpeg command
@@ -29,25 +28,28 @@ function App() {
     // Read the result
     const data = ffmpeg.FS('readFile', 'out.gif');
 
-    //Create a URL
+    // Create a URL
     const url = URL.createObjectURL(new Blob([data.buffer], { type: 'image/gif' }));
-    setGif(url);
-  };
+    setGif(url)
+  }
 
   return ready ? (
+    
     <div className="App">
-      {video && <video
-                  controls
-                  width="250"
-                  src={URL.createObjectURL(video)}>
-                </video>}
+      { video && <video
+        controls
+        width="250"
+        src={URL.createObjectURL(video)}>
+
+      </video>}
+
       <input type="file" onChange={(e) => setVideo(e.target.files?.item(0))} />
 
       <h3>Result</h3>
 
       <button onClick={convertToGif}>Convert</button>
 
-      { gif && <img src={gif} width='250' /> }
+      { gif && <img src={gif} width="250" />}
 
     </div>
   ) : (
