75e59abccb52cae44f560ab36c29982f9b3b258a
sagnikghoshcr7 <sagnikghoshcr7@users.noreply.github.com>
2021-02-19 14:54:36 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
+const ffmpeg = createFFmpeg({ log: true });
+
+function App() {
+
+  return (
+    <div className="App">
+    
+    </div>
+  );
+}
+
+export default App;
